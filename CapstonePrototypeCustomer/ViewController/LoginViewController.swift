425f973dcf3bd7a0637596d220ee6952c4ee2f47
Dhruvil <dhruvilpatel07@icloud.com>
2020-04-09 09:07:33 -0400

    added segue to MainTabController programmatically after user logins

---
+++ CapstonePrototypeCustomer/ViewController/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  CapstonePrototypeCustomer
+//
+//  Created by Dhruvil Patel on 2020-04-09.
+//  Copyright © 2020 Dhruvil Patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        let mainTabController = storyboard?.instantiateViewController(withIdentifier: "MainTabControllerViewController") as! MainTabControllerViewController
+        
+        present(mainTabController, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
