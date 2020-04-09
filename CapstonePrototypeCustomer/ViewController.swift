f2984639c481de0b2eb5034ad39bec70c2467296
Dhruvil <dhruvilpatel07@icloud.com>
2020-04-09 07:52:15 -0400

    Initial Commit

---
+++ CapstonePrototypeCustomer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapstonePrototypeCustomer
+//
+//  Created by Dhruvil Patel on 2020-04-09.
+//  Copyright © 2020 Dhruvil Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
